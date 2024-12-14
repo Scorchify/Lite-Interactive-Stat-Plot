5e7fd6f75c99ed12f0ae451f34b20e0b33265ea3
Alex G <alexg@silversat.org>
2024-12-14 15:58:25 -0500

    adding functions

--- Python/scatter.py
+++ Python/scatter.py
@@ -6,17 +6,18 @@ import pandas
 import numpy as np
 from values import path_to_csv, best_fit, x, y, exponent
 
-sns.set_theme()
-file = pandas.read_csv(path_to_csv)
+def make_scatter():
+    sns.set_theme()
+    file = pandas.read_csv(path_to_csv)
 
-# plot = so.Plot(
-#     data=file,
-#     x="x", y="y"
-# ).add(so.Dot()).add(so.Line(), so.PolyFit())
+    # plot = so.Plot(
+    #     data=file,
+    #     x="x", y="y"
+    # ).add(so.Dot()).add(so.Line(), so.PolyFit())
 
-if best_fit:
-    sns.regplot(file, x=x, y=y)
-else:
-    sns.scatterplot(file, x=x, y=y, order=exponent)
+    if best_fit:
+        sns.regplot(file, x=x, y=y)
+    else:
+        sns.scatterplot(file, x=x, y=y, order=exponent)
 
-p.show()
+    p.show()
