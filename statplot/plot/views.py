5cbe696b51c1f9d6b880fc51fad8720a87115ee2
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-12-14 14:07:41 -0500

    plot 1/2 checkpoint

--- statplot/plot/views.py
+++ statplot/plot/views.py
@@ -1,8 +1,34 @@
 from django.shortcuts import render
+import csv
+from django.http import HttpResponse
+from .forms import DataInputFormSet
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def graph(request):
-    return render(request, 'plot.html')
+    return render(request, 'plot.html') 
+
+def data_view(request):
+    if request.method == "POST":
+        formset = DataInputFormSet(request.POST)
+        if formset.is_valid():
+            data = formset.cleaned_data
+            response = HttpResponse(content_type='text/csv')
+            response['Content-Disposition'] = 'attachment; filename="data.csv"'
+            writer = csv.writer(response)
+            writer.writerow(['x', 'y', 'color', 'point_type', 'group'])
+            
+            for form in formset: 
+                writer.writerow([
+                    form.cleaned_data['x'],
+                    form.cleaned_data['y'],
+                    form.cleaned_data['color'],
+                    form.cleaned_data['point_type'],
+                    form.cleaned_data['group']
+                ])
+                return response
+            else:
+                formset = DataInputFormSet()
+            return render(request, 'data.html', {'formset': formset})
