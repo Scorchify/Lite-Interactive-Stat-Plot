df1fce8197cc7ab3431f709f4fae712990023a35
Alex G <alexg@silversat.org>
2024-12-14 12:29:08 -0500

    Scatterplot with regression
    Co-authored-by: frownsty <frownsty@users.noreply.github.com>

--- Python/init.py
+++ Python/init.py
@@ -1,16 +1,17 @@
 # Import seaborn
 import matplotlib.pyplot as plot
 import seaborn as sns
+import seaborn.objects as so
 import pandas
 
-# Apply the default theme
 sns.set_theme()
+file = pandas.read_csv("data.csv")
 
-# Load an example dataset
-tips = pandas.read_csv("data.csv")
+so.Plot(
+    data=file,
+    x="x", y="y"
+).add(so.Dot())
 
-# Create a visualization
-sns.relplot(
-    data=tips
-)
+sns.lmplot(x="x", y="y", data=file)
+
 plot.show()
