fb3df2506298e0e177520090ff934d1e7c64f97d
Alex G <alexg@silversat.org>
2024-12-14 15:45:46 -0500

    implamenting

--- Python/histogram.py
+++ Python/histogram.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plot # type: ignore
 import seaborn as sns # type: ignore
 import seaborn.objects as so # type: ignore
 import pandas # type: ignore
+from values import path_to_csv, x
 
-file = pandas.read_csv("data_histogram.csv")
+file = pandas.read_csv(path_to_csv)
 
 #line overlay
 #sns.displot(data=file, x="flipper_length_mm", kind="kde", bw_adjust=2)
 
-p = so.Plot(file,"flipper_length_mm")
+p = so.Plot(file,x)
 p2 = p.add(so.Bars(alpha=.3), so.Hist("density")).add(so.Line(), so.KDE())
 p2.show()
 
--- Python/scatter.py
+++ Python/scatter.py
@@ -4,15 +4,19 @@ import seaborn as sns
 import seaborn.objects as so
 import pandas
 import numpy as np
+from values import path_to_csv, best_fit, x, y
 
 sns.set_theme()
-file = pandas.read_csv("data_scatter.csv")
+file = pandas.read_csv(path_to_csv)
 
 # plot = so.Plot(
 #     data=file,
 #     x="x", y="y"
 # ).add(so.Dot()).add(so.Line(), so.PolyFit())
 
-sns.regplot(file, x="x", y="y")
+if best_fit:
+    sns.regplot(file, x=x, y=y)
+else:
+    sns.scatterplot(file, x=x, y=y)
 
 p.show()
