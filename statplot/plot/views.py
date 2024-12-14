b7e2660c8276ca0b5c6a7e8ce602df241381422f
Scorchify <139656419+Scorchify@users.noreply.github.com>
2024-12-14 16:14:32 -0500

    ok

--- statplot/plot/views.py
+++ statplot/plot/views.py
@@ -10,28 +10,48 @@ import matplotlib
 import pandas as pd
 matplotlib.use('Agg')
 from io import BytesIO
-import base64 
+import base64
+import os
+from django.conf import settings
 
-
+# Variables
+x = "velocity"
+y = "time"
+exponent = 1
+regressionType = "polynomial"
+path_to_csv = ""
+best_fit = True
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def graph(request):
+    global path_to_csv  # Declare the variable as global to modify it
     if request.method == 'POST':
         form = DataInputForm(request.POST, request.FILES)
         if form.is_valid():
             csv_file = request.FILES.get('csv_file')
             if csv_file:
                 try:
-                    data = pd.read_csv(csv_file)
+                    temp_file_path = os.path.join(settings.MEDIA_ROOT, csv_file.name)
+                    with open(temp_file_path, 'wb+') as destination:
+                        for chunk in csv_file.chunks():
+                            destination.write(chunk)
+
+                    path_to_csv = temp_file_path
+
+                    # Read the CSV file
+                    data = pd.read_csv(temp_file_path)
                 except pd.errors.ParserError:
                     form.add_error('csv_file', 'The CSV file is formatted incorrectly.')
                     return render(request, 'plot.html', {'form': form})
                 
                 plt.figure(figsize=(10, 6))
-                sns.regplot(data=data, x='x', y='y')
+                if best_fit:
+                    sns.regplot(data=data, x=x, y=y)
+                else:
+                    sns.scatterplot(data=data, x=x, y=y)
                 buffer = BytesIO()
                 plt.savefig(buffer, format='png')
                 buffer.seek(0)
@@ -40,8 +60,7 @@ def graph(request):
                 image_base64 = base64.b64encode(image_png).decode('utf-8')
                 return render(request, 'plot.html', {'form': form, 'graph': image_base64})
             else:
-                form.add_error('csv_file', 'This field is required.')
+                form.add_error('csv_file', 'Required Field.')
     else:
         form = DataInputForm()
     return render(request, 'plot.html', {'form': form})
-
